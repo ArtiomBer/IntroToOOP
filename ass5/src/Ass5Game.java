50e1aae25d01bb5ca59be73b4aa21f5a4b7c2809
Artiom Berengard <99901151+ArtiomBer@users.noreply.github.com>
2022-09-15 10:44:49 +0300

    Add files via upload

---
+++ ass5/src/Ass5Game.java
@@ -0,0 +1,19 @@
+// Artiom Berengard 321114704
+import Sprites.Game;
+
+/**
+ * This class has the main method, it creates, initializes and runs
+ * the game.
+ */
+public class Ass5Game {
+    /**
+     * The method will start a new game, initialize and run it.
+     * @param args are the default arguments, currently they are
+     *             not used.
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initialize();
+        game.run();
+    }
+}
